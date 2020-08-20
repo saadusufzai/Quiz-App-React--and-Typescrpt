9dbaf3bf665b54250bb0f248bea21eb440dfc221
Saad Aslam <saadusufzai@gmail.com>
2020-08-20 19:55:04 +0500

    getting data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import {getQuizDetails} from './services/service'
+import { data } from './types/types';
 
 function App() {
   useEffect(()=>{
     const fetch =async ()=>{
-    const data =  await getQuizDetails(5,10,'easy')
+    const data:data =  await getQuizDetails(5,10,'easy')
     console.log(data)
   }
   fetch()
---
+++ src/types/types.tsx
@@ -0,0 +1,14 @@
+ export type data = {
+    category: string
+    correct_answer: string
+    difficulty: string
+    incorrect_answers: string[]
+    question: string
+    type: string
+ }
+
+ export type quiz ={
+     question:string 
+     answer:string
+     options:string[]
+ }
