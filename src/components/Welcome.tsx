50af5dac6db07c4c2a930b8ad372d6727d285804
Saad Aslam <saadusufzai@gmail.com>
2020-08-22 18:44:43 +0500

    Welcome Css completed
    Color left

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,7 +4,12 @@ import { categories } from "../services/service";
 import { welcomeProp } from "../types/types";
 import QuestionUi from "./QuestionUi";
 
-const Welcome: React.FC<welcomeProp> = ({ setCategory, setTotalQuestions, setLevel,setStart }) => {
+const Welcome: React.FC<welcomeProp> = ({
+  setCategory,
+  setTotalQuestions,
+  setLevel,
+  setStart,
+}) => {
   const [name, setName] = useState<string[]>();
 
   const [selCategory, setSelectedCategory] = useState<string>(
@@ -32,63 +37,73 @@ const Welcome: React.FC<welcomeProp> = ({ setCategory, setTotalQuestions, setLev
     setLevel(e.target.value);
   };
 
-  let handelSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault()
-    setStart(true)
-  }
+  let handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setStart(true);
+  };
 
   return (
-    <div>
-      <h1>Welcome to the Ultimate Quiz App</h1>
-      <p>Select your ultimate quiz and test your knowledge </p>
+    <div className="welcome">
+      <div className="heading">
+        <h1>Welcome to the Ultimate Quiz App</h1>
+        <p>Select your ultimate quiz and test your knowledge </p>
+      </div>
       <div>
-        <form  onSubmit={(e: React.FormEvent<HTMLFormElement>)=>{handelSubmit(e)}}>
-          <div>
-            <h3>Catagories</h3>
-            <select
-              name="category"
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                handelCategory(e)
-              }
-            >
-              {name?.map((e, i) => {
-                return (
-                  <option key={i} value={e}>
-                    {e}
-                  </option>
-                );
-              })}
-            </select>
+        <form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+            handelSubmit(e);
+          }}
+        >
+          <div className="form">
+            <div className='box'>
+              <h3>Catagories</h3>
+              <select
+                name="category"
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  handelCategory(e)
+                }
+              >
+                {name?.map((e, i) => {
+                  return (
+                    <option key={i} value={e}>
+                      {e}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className='box'>
+              <h3>Total Questions</h3>
+              <select
+                name="ammount"
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  handelAmmount(e)
+                }
+              >
+                <option value={5}>5</option>
+                <option value={10}>10</option>
+                <option value={15}>15</option>
+                <option value={20}>20</option>
+                <option value={25}>25</option>
+              </select>
+            </div>
+            <div className='box'>
+              <h3>Difficulty Level</h3>
+              <select
+                name="difficulty"
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  handelDifficulty(e)
+                }
+              >
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
           </div>
-          <div>
-            <h3>Total Questions</h3>
-            <select
-              name="ammount"
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                handelAmmount(e)
-              }
-            >
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={15}>15</option>
-              <option value={20}>20</option>
-              <option value={25}>25</option>
-            </select>
+          <div className="startQuiz">
+            <input type="submit" value="START QUIZ" />
           </div>
-          <div>
-            <h3>Difficulty Level</h3>
-            <select
-              name="difficulty"
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-                handelDifficulty(e)
-              }
-            >
-              <option value='easy'>Easy</option>
-              <option value='medium'>Medium</option>
-              <option value='hard'>Hard</option>
-            </select>
-          </div>
-          <div><input type="submit" value="START QUIZ"/></div>
         </form>
       </div>
     </div>
